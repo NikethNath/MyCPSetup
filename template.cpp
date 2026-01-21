a41fd727863b4e95826efb0e683b7c59e04ceddc
NikethNath <nikethnath@gmail.com>
2026-01-21 23:18:56 +0530

    Initial commit: Competitive Programming setup with IO redirection

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;    
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+#define pb push_back
+const int inf = 1e9;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        
+    }
+
+    
+}
