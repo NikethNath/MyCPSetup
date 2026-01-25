f104812791ef05d305be9f60e2ce52f266018723
NikethNath <nikethnath@gmail.com>
2026-01-26 00:15:03 +0530

    improved code readability and added a snippets file for vscode

--- template.cpp
+++ template.cpp
@@ -8,24 +8,15 @@ using vi = vector<int>;
 using vll = vector<ll>;
 using vpii = vector<pii>;
 using vpll = vector<pll>;
-using pqi=priority_queue<int>;
-using pqpii=priority_queue<pii>;
+using pqi = priority_queue<int>;
+using pqpii = priority_queue<pii>;
 
 #define pb push_back
-#define all(x) (x).begin(),(x).end()
+#define all(x) (x).begin(), (x).end()
 const int inf = 1e9;
-const ll INF=1e18;
+const ll INF = 1e18;
 
-bool comp(pii a, pii b ){
-    if(a.second<b.second) return true;
-    return false;
-}
-int bs(vi &a,int target){
-    auto it=upper_bound(a.begin(),a.end(),target);   //first element> target
-    //auto it=lower_bound(a.begin(),a.end(),target);    //first element>= target
-    int index=distance(a.begin(),it);
-    return index;
-}
+
 
 int main() {
     #ifndef ONLINE_JUDGE
@@ -33,12 +24,10 @@ int main() {
         freopen("output.txt", "w", stdout);
     #endif
 
-
     int t;
-    cin>>t;
-    for(int i=0;i<t;i++){
-        
+    cin >> t;
+    for(int i=0 ; i<t ; i++) {
+        //code
     }
-
-    
+    return 0;
 }
