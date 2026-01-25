2fdc23562b3bf04e90bd75fc232068ce6c2435f4
NikethNath <nikethnath@gmail.com>
2026-01-25 23:50:31 +0530

    added some extra utilities like binarysearch, all(x) in template

--- template.cpp
+++ template.cpp
@@ -8,13 +8,23 @@ using vi = vector<int>;
 using vll = vector<ll>;
 using vpii = vector<pii>;
 using vpll = vector<pll>;
+using pqi=priority_queue<int>;
+using pqpii=priority_queue<pii>;
 
 #define pb push_back
+#define all(x) (x).begin(),(x).end()
 const int inf = 1e9;
+const ll INF=1e18;
 
 bool comp(pii a, pii b ){
     if(a.second<b.second) return true;
     return false;
+}
+int bs(vi &a,int target){
+    auto it=upper_bound(a.begin(),a.end(),target);   //first element> target
+    //auto it=lower_bound(a.begin(),a.end(),target);    //first element>= target
+    int index=distance(a.begin(),it);
+    return index;
 }
 
 int main() {
