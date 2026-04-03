ca33ec52c10839a1449a8fb788f33ca6bfd57f31
Niketh <nikethnath@gmail.com>
2026-04-03 11:21:33 +0530

    cleaned up existing data in expected.txt,input.txt,output.txt and added seperate files for contests

---
+++ C.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;    
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pqi = priority_queue<int>;
+using pqpii = priority_queue<pii>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+const int inf = 1e9;
+const ll INF = 1e18;
+
+
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    for(int i=0 ; i<t ; i++) {
+        //code
+    }
+    return 0;
+}
